3984422aa6c0807720137c81514087c207cad49c
Nevzat <nevzatcelik151907@gmail.com>
2023-04-17 12:55:56 +0200

    first lesson

--- src/test/java/DenemeTesti.java
+++ src/test/java/DenemeTesti.java
@@ -11,16 +11,20 @@ import java.util.concurrent.TimeUnit;
 public class DenemeTesti {
 
     AndroidDriver<MobileElement> driver;
+   final String cihazAdi="PIXEL";
+   final String platformIsmi="Android";
+   final String version="10.0";
+   final String automation="UiAutomator2";
 
     @Test
     public void deneme1() throws MalformedURLException {
 
         DesiredCapabilities capabilities=new DesiredCapabilities();
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"PIXEL");
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10.0");
-        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
-        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\ahmet\\IdeaProjects\\Appium_T-108\\Apps\\Calculator_8.4 (503542421)_Apkpure (1).apk");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,cihazAdi);
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,platformIsmi);
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,version);
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,automation);
+        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\ahmet\\IdeaProjects\\Appium_T-108\\Apps\\Calculator_8.4 (503542421)_Apkpure.apk");
 
         driver=new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
