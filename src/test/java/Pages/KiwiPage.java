edbcfb443af0bedc4d3fd3aa2885cf0a3177d7fd
Nevzat <nevzatcelik151907@gmail.com>
2023-04-19 12:28:12 +0200

    third lesson

--- src/test/java/Pages/KiwiPage.java
+++ src/test/java/Pages/KiwiPage.java
@@ -1,16 +1,31 @@
 package Pages;
 
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utils.Driver;
 
+import java.time.Duration;
+
 public class KiwiPage {
     public KiwiPage(){
         PageFactory.initElements((WebDriver) Driver.getAndroidDriver(),this);
     }
 
-    @FindBy(xpath = "")
-    public WebElement ahmet;
+    @FindBy(xpath = "//*[@text='Continue as a guest']")
+    public WebElement asAGuest;
+
+    public void ucButtonTiklama(){
+        TouchAction action=new TouchAction<>(Driver.getAndroidDriver());
+        for (int i=0; i<3; i++){
+            action.press(PointOption.point(538,1686)).
+                    waitAction(WaitOptions.waitOptions(Duration.ofMillis(500)))
+                    .release().
+                    perform();
+        }
+    }
 }
--- src/test/java/tests/KiwiTest.java
+++ src/test/java/tests/KiwiTest.java
@@ -1,10 +1,17 @@
 package tests;
 
 import Pages.KiwiPage;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.Driver;
+
+import java.time.Duration;
 
 public class KiwiTest {
     // uygulamanin yuklendigi dogrulanir
@@ -24,6 +31,18 @@ public class KiwiTest {
     KiwiPage page=new KiwiPage();
     @Test
     public void kiwiTest(){
+        // uygulamanin yuklendigi dogrulanir
+        Assert.assertTrue(driver.isAppInstalled("com.skypicker.main"),"uygulama yuklenemedi");
+        // uygulamanin basariyla acildigi dogrulanir
+       String yaziTextActual= page.asAGuest.getText();
+       String yaziTextExpected="Continue as a guest";
+       Assert.assertEquals(yaziTextActual,yaziTextExpected,"uygulama basarili bir sekilde baslatilamadi");
+        // misafir olarak devam et e tiklanir
+        page.asAGuest.click();
+        // ardinda gelecek olan 3 adimada yesil butona basilarak devam edilir
+        page.ucButtonTiklama();
+
+
 
     }
 }
