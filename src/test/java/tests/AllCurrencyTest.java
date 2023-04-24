e684aa527166f3f5b1ac79a5170f622bd173a5e9
Nevzat <nevzatcelik151907@gmail.com>
2023-04-24 12:17:41 +0200

    fourth lesson

--- src/test/java/tests/AllCurrencyTest.java
+++ src/test/java/tests/AllCurrencyTest.java
@@ -2,13 +2,17 @@ package tests;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.Driver;
 import utils.ReusableMethods;
 
 import java.awt.geom.RectangularShape;
+import java.io.File;
+import java.io.IOException;
 
 public class AllCurrencyTest {
 
@@ -23,7 +27,7 @@ public class AllCurrencyTest {
 AndroidDriver<AndroidElement> driver= Driver.getAndroidDriver();
 
     @Test
-    public void currencyTest(){
+    public void currencyTest() throws IOException {
         // all currency uygulamasinin yuklendigi dogulanir
         Assert.assertTrue(driver.isAppInstalled("com.smartwho.SmartAllCurrencyConverter"),"uygulama yukleme basarisiz");
         // uygulamanin acildigi dogrulanir
@@ -35,13 +39,16 @@ AndroidDriver<AndroidElement> driver= Driver.getAndroidDriver();
         // cevirmek istedigimiz para birimi zloty olarak secilir
       //  driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"PLN\"))");
         ReusableMethods.scrollWithUiScrollable("PLN");
-
+     // cevirelecek tutari tuslayalim
         driver.findElement(By.id("com.smartwho.SmartAllCurrencyConverter:id/SpinnerCurrencyB")).click();
         ReusableMethods.scrollWithUiScrollable("Turkish Lira");
         driver.findElement(By.id("com.smartwho.SmartAllCurrencyConverter:id/b1")).click();
         driver.findElement(By.id("com.smartwho.SmartAllCurrencyConverter:id/b5")).click();
         driver.findElement(By.id("com.smartwho.SmartAllCurrencyConverter:id/b0")).click();
         driver.findElement(By.id("com.smartwho.SmartAllCurrencyConverter:id/b0")).click();
+        // cevrilen tutar screenShot olarak kaydedilir
+        File ekranfotografi=driver.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(ekranfotografi,new File("paraSonucu.jpg"));
 
 
     }
